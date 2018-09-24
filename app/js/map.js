1a1747f50bcbafb7b460f3c032451a33e98a88f9
em <6em.rva@gmail.com>
2018-09-24 15:40:14 -0400

    map shows party colors

--- app/js/map.js
+++ app/js/map.js
@@ -5,9 +5,9 @@ var mymap = L.map('mapid').setView([38.307989, -79.269187], 7);
 function partyColor(party) {
     switch (party) {
         case "Democratic":
-        return "blue";
+            return "blue";
         case "Republican":
-        return "red";
+            return "red";
     }
 }
 
@@ -17,68 +17,77 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZWxtOSIsImEiOiJjam05Yzl4a281MHhhM2tvZ3M1ZmtqaXRiIn0.KigR8xW7dn9FGF2TmDsoaw'
-    }).addTo(mymap);
-    
+}).addTo(mymap);
+
 
 // geoJSON congressional districts with a popup
-var districts = $.getJSON("./assets/JSON/map.geojson", function(data){
-    
-    
+var districts = $.getJSON("./assets/JSON/map.geojson", function (data) {
+
+
     // new layer for geojson layer
     L.geoJson(data, {
-        style: function(feature){
-            return{
-                // set opacity to 40%
-                fillOpacity: 0.4,
-                // set border weight to 1.5px
-                weight: 1.5
-            };
-        },
+
         // call on each feature
-        onEachFeature: function(feature, layer){
-            
-            // create variable for OCDid
+        onEachFeature: function (feature, layer) {
+
+        // -----------------------------------------------------------------------------------------
+            // here's the Google Civics Info API call
+        // -----------------------------------------------------------------------------------------
+            // variable for OCDid found in map.geojson
             var OCDid = feature.properties.OCDid;
-            //API key yes its bad to put this here 
-            var api = "?key=AIzaSyBEexgjdV_RBhX9PecP2O8sZxRbGzB9pPA/";
-            // get google civic info api link and store in variable called civicInfoLink
-            var civicInfoLink = "https://www.googleapis.com/civicinfo/v2/representatives" + api + OCDid;
+            //API key, yes its bad to put this here ¯\_(ツ)_/¯
+            var api = "?key=AIzaSyBEexgjdV_RBhX9PecP2O8sZxRbGzB9pPA";
+            // store google civic info api link in variable called civicInfoLink
+            var civicInfoLink = "https://www.googleapis.com/civicinfo/v2/representatives/" + OCDid + api;
             console.log(OCDid);
             console.log(civicInfoLink);
+            // here's where we call the API itself, the data is stored in data2
+            $.ajax({
+                url: civicInfoLink,
+                type: "GET",
+                dataType: "json",
+                success: function (data2) {
+                    console.log(data2);
+                    // set the fill color based on the party color using my partyColor function above
+                    layer.setStyle({
+                        fillColor: partyColor(data2.officials[0].party)
+                    })
+                }
+            }),
+        // -----------------------------------------------------------------------------------------
+        console.log(data);  
+        console.log(feature.properties.District);
             layer.on({
                 // function on mouseover 
-                mouseover: function(event) {
+                mouseover: function (event) {
                     layer = event.target;
                     // change opacity to 80% to show user a difference in layer
                     layer.setStyle({
                         fillOpacity: 0.8
                     });
                     // popup shows which district it is
-                    layer.bindPopup("<h1>"+feature.properties.District+" District</h1>");
+                    layer.bindPopup("<h4>" + feature.properties.District + " Congressional District</h4>");
                 },
                 //function when mouse no longer is hovering over location
-                mouseout: function(event) {
+                mouseout: function (event) {
                     layer = event.target
                     // return opacity to 40%
                     layer.setStyle({
-                        fillOpacity: 0.4
+                        fillOpacity: 0.6
                     });
                 }
+
             });
 
         },
+        style: function (feature) {
 
-
+            return {
+                // set opacity to 40%
+                fillOpacity: 0.6,
+                // set border weight to 1.5px
+                weight: 1.5,
+            };
+        },
     }).addTo(mymap);
-    console.log(L.geoJson(data));
-    
-    
-    });
-// add google civic API data to map
-    // link to API
-    
-
-
-
-
-    
+});
